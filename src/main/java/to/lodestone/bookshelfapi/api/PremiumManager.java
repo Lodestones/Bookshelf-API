58e61a44d3caf102a9f84ff7100a29bdd30f299c
John Aquino <41652412+apollyon600@users.noreply.github.com>
2024-10-07 05:13:35 -0700

    feat: v1.0.9

--- src/main/java/to/lodestone/bookshelfapi/api/PremiumManager.java
+++ src/main/java/to/lodestone/bookshelfapi/api/PremiumManager.java
@@ -7,11 +7,48 @@ import java.net.URL;
 
 public class PremiumManager implements Listener {
     private final static String URL = "https://lodestone.to/api/premium/verify";
+    private final static String LICENSE_URL = "https://lodestone.to/api/license/verify";
 
     private final boolean isPremiumServer;
+    private boolean isLicensedServer;
 
-    public PremiumManager(String ip) {
-        this.isPremiumServer = isPremiumServer(URL + String.format("?ip=%s", ip));
+    @SuppressWarnings("deprecation")
+    public PremiumManager() {
+        boolean isPremiumServer;
+        try {
+            URL url = new URL("https://api.ipify.org");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+            String ip = new String(conn.getInputStream().readAllBytes());
+            isPremiumServer = checkStatus(URL + String.format("?ip=%s", ip));
+        } catch (Exception ignored) {
+            isPremiumServer = false;
+        }
+        this.isPremiumServer = isPremiumServer;
+        this.isLicensedServer = false;
+    }
+
+    @SuppressWarnings("deprecation")
+    public PremiumManager(String licenseKey) {
+        this();
+        boolean isLicensedServer;
+        try {
+            URL url = new URL("https://api.ipify.org");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+            String ip = new String(conn.getInputStream().readAllBytes());
+            isLicensedServer = checkStatus(LICENSE_URL + String.format("?ip=%s&key=%s", ip, licenseKey));
+        } catch (Exception ignored) {
+            isLicensedServer = false;
+        }
+
+        this.isLicensedServer = isLicensedServer;
+    }
+
+    public boolean isLicensedServer() {
+        return isLicensedServer;
     }
 
     public boolean isPremiumServer() {
@@ -19,7 +56,7 @@ public class PremiumManager implements Listener {
     }
 
     @SuppressWarnings("deprecation")
-    private boolean isPremiumServer(String urlString) {
+    private boolean checkStatus(String urlString) {
         try {
             URL url = new URL(urlString);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
