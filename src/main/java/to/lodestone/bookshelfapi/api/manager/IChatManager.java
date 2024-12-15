0edd76446c8c4a08ba8aae7f68260acd0615c625
John Aquino <41652412+apollyon600@users.noreply.github.com>
2024-12-14 23:44:48 -0700

    feat: added canPlayerSpeak method to IChatManager

--- src/main/java/to/lodestone/bookshelfapi/api/manager/IChatManager.java
+++ src/main/java/to/lodestone/bookshelfapi/api/manager/IChatManager.java
@@ -1,8 +1,11 @@
 package to.lodestone.bookshelfapi.api.manager;
 
+import java.util.UUID;
+
 public interface IChatManager {
 
     void setChatMuted(boolean value);
     boolean isChatMuted();
+    boolean canPlayerSpeak(UUID uniqueId);
 
 }
