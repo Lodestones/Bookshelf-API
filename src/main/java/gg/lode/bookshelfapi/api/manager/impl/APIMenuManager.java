47bb07cd8eb8d89bf085123f39e192365fd93194
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-05-20 16:40:37 -0700

    feat(menu): implement menu management system

--- src/main/java/gg/lode/bookshelfapi/api/manager/impl/APIMenuManager.java
+++ src/main/java/gg/lode/bookshelfapi/api/manager/impl/APIMenuManager.java
@@ -4,6 +4,7 @@ import gg.lode.bookshelfapi.api.Task;
 import gg.lode.bookshelfapi.api.manager.IMenuManager;
 import gg.lode.bookshelfapi.api.menu.Menu;
 import gg.lode.bookshelfapi.api.menu.build.MenuBuilder;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,24 +13,24 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.UUID;
 
 public class APIMenuManager implements IMenuManager, Listener {
-
-    private final JavaPlugin plugin;
     private static final HashMap<UUID, Menu> activeMenus = new HashMap<>();
+    private final JavaPlugin plugin;
 
     public APIMenuManager(JavaPlugin plugin) {
         this.plugin = plugin;
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        plugin.getServer().getPluginManager().registerEvents(this, (Plugin) plugin);
     }
 
     @Override
     public void register(Player player, Menu menu) {
-        register(player.getUniqueId(), menu);
+        this.register(player.getUniqueId(), menu);
     }
 
     @Override
@@ -43,36 +44,38 @@ public class APIMenuManager implements IMenuManager, Listener {
 
     @Override
     public void registerAndOpen(Player player, Menu menu) {
-        registerAndOpen(player.getUniqueId(), menu);
+        this.registerAndOpen(player.getUniqueId(), menu);
     }
 
     @Override
     public void registerAndOpen(UUID uniqueId, Menu menu) {
-        register(uniqueId, menu);
+        this.register(uniqueId, menu);
         menu.open();
     }
 
     @EventHandler
     public void on(InventoryClickEvent event) {
-        if (!(event.getWhoClicked() instanceof Player player)) return;
-
-        Inventory clickedInventory = event.getClickedInventory();
-        if (clickedInventory == null)
+        HumanEntity humanEntity = event.getWhoClicked();
+        if (!(humanEntity instanceof Player)) {
             return;
-
+        }
+        Player player = (Player) humanEntity;
+        Inventory clickedInventory = event.getClickedInventory();
+        if (clickedInventory == null) {
+            return;
+        }
         InventoryView view = event.getView();
-
         if (activeMenus.containsKey(player.getUniqueId())) {
-            final int slot = event.getSlot();
+            int slot = event.getSlot();
             Menu menu = activeMenus.get(player.getUniqueId());
-
             if (view.getTopInventory().equals(clickedInventory)) {
                 menu.getTopMenuBuilder().getClickActions().forEach(c -> c.accept(event));
                 menu.getTopMenuBuilder().process(slot, event);
             } else if (view.getBottomInventory().equals(clickedInventory)) {
                 MenuBuilder bottomMenuBuilder = menu.getBottomMenuBuilder();
-                if (bottomMenuBuilder == null) return;
-
+                if (bottomMenuBuilder == null) {
+                    return;
+                }
                 bottomMenuBuilder.process(slot, event);
             }
         }
@@ -80,18 +83,23 @@ public class APIMenuManager implements IMenuManager, Listener {
 
     @EventHandler
     public void on(InventoryCloseEvent event) {
-        if (!(event.getPlayer() instanceof Player player)) return;
-
-        Menu menu = activeMenus.get(player.getUniqueId());
-        if (menu == null)
+        HumanEntity humanEntity = event.getPlayer();
+        if (!(humanEntity instanceof Player)) {
             return;
-
+        }
+        Player player = (Player) humanEntity;
+        Menu menu = activeMenus.get(player.getUniqueId());
+        if (menu == null) {
+            return;
+        }
         menu.getTopMenuBuilder().getCloseActions().forEach(closeEvent -> closeEvent.accept(event));
-        if (!event.getReason().equals(InventoryCloseEvent.Reason.OPEN_NEW)) Task.later(plugin, () -> activeMenus.remove(player.getUniqueId()), 1L);
+        if (!event.getReason().equals(InventoryCloseEvent.Reason.OPEN_NEW)) {
+            Task.later(this.plugin, () -> activeMenus.remove(player.getUniqueId()), 1L);
+        }
     }
 
     @EventHandler
     public void on(PlayerQuitEvent event) {
         activeMenus.remove(event.getPlayer().getUniqueId());
     }
-} 
+}
