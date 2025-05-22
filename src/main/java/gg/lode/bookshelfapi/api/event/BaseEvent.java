8533001ede2f70d129d916d4fef16b8cc8c49b7b
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-05-21 23:37:23 -0700

    feat(api): move events to events-api and kept BaseEvent.java

--- src/main/java/gg/lode/bookshelfapi/api/event/BaseEvent.java
+++ src/main/java/gg/lode/bookshelfapi/api/event/BaseEvent.java
@@ -1,37 +1,29 @@
 package gg.lode.bookshelfapi.api.event;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * This parent class structures future possible classes other people would like to create.
- *
- * @author John Aquino
- */
+
 public abstract class BaseEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
 
+    public BaseEvent() {
+        super();
+    }
+
+    public BaseEvent(boolean isAsync) {
+        super(isAsync);
+    }
+
+    @NotNull
     public static HandlerList getHandlerList() {
         return handlers;
     }
 
-    public @NotNull HandlerList getHandlers() {
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
         return handlers;
     }
-
-    public BaseEvent() {
-        this(false);
-    }
-
-    public BaseEvent(boolean async) {
-        super(async);
-    }
-
-    public boolean call() {
-        Bukkit.getPluginManager().callEvent(this);
-        return this instanceof Cancellable && ((Cancellable)this).isCancelled();
-    }
-}
+} 
