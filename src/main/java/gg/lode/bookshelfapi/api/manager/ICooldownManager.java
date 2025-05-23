1724a6d443e4195e711b9f9eb8b80bc025a4b1f0
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-05-22 17:16:22 -0700

    feat(api): callbacks to when cooldowns are completed.

--- src/main/java/gg/lode/bookshelfapi/api/manager/ICooldownManager.java
+++ src/main/java/gg/lode/bookshelfapi/api/manager/ICooldownManager.java
@@ -3,17 +3,28 @@ package gg.lode.bookshelfapi.api.manager;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 
+import java.util.function.Consumer;
+
 public interface ICooldownManager {
 
     void setCooldown(Player player, String id, long milliseconds);
+
     void setCooldown(String id, long milliseconds);
 
+    void setCooldown(Player player, String id, long milliseconds, Consumer<Player> callback);
+
+    void setCooldown(String id, long milliseconds, Consumer<Player> callback);
+
     boolean hasCooldown(Player player, String id);
+
     boolean hasCooldown(String id);
 
     boolean notifyPlayerWithCooldown(Player player, String id, Component component);
+
     boolean notifyPlayerWithCooldown(Player player, String id, String message);
+
     boolean notifyPlayerWithCooldown(Player player, String id, Component component, long milliseconds);
+
     boolean notifyPlayerWithCooldown(Player player, String id, String message, long milliseconds);
 
     long getCooldown(Player player, String id);
